83ad42d998e4c59d4dd8d4999cd6a6512bce0d20
Will MacLean <maclean.will@gmail.com>
2025-12-17 12:04:08 -0600

    basic oscillators

--- metronome/src/main.tsx
+++ metronome/src/main.tsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const audioContext = new AudioContext()
+const gainNode = audioContext.createGain();
+gainNode.gain.value = .004
+gainNode.connect(audioContext.destination)
+
+const oscillatorDownBeat = audioContext.createOscillator();
+oscillatorDownBeat.type = 'sawtooth'
+oscillatorDownBeat.frequency.value = 220
+oscillatorDownBeat.connect(gainNode)
+
+const oscillatorWeakBeat = audioContext.createOscillator();
+oscillatorWeakBeat.frequency.value = 110
+oscillatorWeakBeat.type = 'square'
+oscillatorWeakBeat.connect(gainNode)
+
+
+// oscillatorDownBeat.start()
+
+
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
