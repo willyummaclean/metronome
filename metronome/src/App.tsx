361a2eb4209b992de2532d09eb34dd0a27c3e56d
Will MacLean <maclean.will@gmail.com>
2025-12-17 18:44:38 -0600

    one beep :/

--- metronome/src/App.tsx
+++ metronome/src/App.tsx
@@ -1,11 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [bpm, setBpm] = useState(40)
-  const [timeSig, setTimeSig] = useState("")
+  const [timeSig, setTimeSig] = useState(0)
+  const [clickOn, setClickOn] = useState(false)
+  let beatNumber = 1
+  let nextNoteTime = 0.0;  
+  const scheduleAheadTime = 0.1;  
+
+  const audioContext = new AudioContext()
+  const gainNode = audioContext.createGain();
+  gainNode.gain.value = .004
+  gainNode.connect(audioContext.destination)
+
+  function nextNote() {
+    const secondsPerBeat = 60.0 / bpm;    
+    nextNoteTime += secondsPerBeat;
+    if (beatNumber > timeSig) {
+        beatNumber = 1
+      }
+      else {
+        beatNumber++
+      }
+    }
+
+  function scheduleClick(beatNumber: number, time: number) {  
+      const osc = audioContext.createOscillator();
+      osc.connect(gainNode);
+      if (beatNumber === 1) {
+          osc.type = 'sawtooth'
+          osc.frequency.value = 220;
+      } else {
+          osc.type = 'square'
+          osc.frequency.value = 110.0;
+      }
+      osc.start( time );
+      osc.stop( time + .05 );     
+  }
+  
+  function scheduler() {
+      while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
+          scheduleClick( beatNumber, nextNoteTime );
+          nextNote();
+      }
+  }
+
+  useEffect(() => {
+    if (clickOn){
+    scheduler()
+    }
+  })
+ 
 
   return (
     <>
@@ -26,16 +74,16 @@ function App() {
           Selected BPM: {bpm}
           </p>
         <label>4/4:
-        <input onChange={() => setTimeSig("4/4")} type="radio" id="4/4" name="timeSignature" value="4/4"/>
+        <input onChange={() => setTimeSig(4)} type="radio" id="4/4" name="timeSignature" value="4/4"/>
         </label> 
         <label>3/4:
-        <input onChange={() => setTimeSig("3/4")} type="radio" id="3/4" name="timeSignature" value="3/4"/>
+        <input onChange={() => setTimeSig(3)} type="radio" id="3/4" name="timeSignature" value="3/4"/>
         </label>
         <p>
           Selected Time Signature: {timeSig}
         </p>
-        <button id="startButton">Start</button>
-        <button id="stopButton">Stop</button>
+        <button onClick={() => setClickOn(true)}id="startButton" >Start</button>
+        <button onClick={() => setClickOn(false)} id="stopButton">Stop</button>
       
       </div>
       <p className="read-the-docs">
@@ -44,5 +92,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
